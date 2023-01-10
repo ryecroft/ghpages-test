18a68250e7310fe26c0fcb43259c88e0a549465c
fatboy <stephen@fatmail.fr>
2023-01-10 12:01:31 +0100

    show user cookie

--- input-cookie.js
+++ input-cookie.js
@@ -1,3 +1,4 @@
 document.querySelector( '#input' ).addEventListener( 'input', function ( e ) {
   document.cookie = "ukcsid=" + e.target.value.replace( /ukcsid=/g, "" )
+  document.querySelector( '#cookie-value' ).innerHTML = document.cookie
 } )
