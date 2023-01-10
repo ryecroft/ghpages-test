b85008206c2bd3ff8fc5ec09905c5e7714a1790e
fatboy <stephen@fatmail.fr>
2023-01-10 12:05:08 +0100

    update sw.js

--- input-cookie.js
+++ input-cookie.js
@@ -2,3 +2,7 @@ document.querySelector( '#input' ).addEventListener( 'input', function ( e ) {
   document.cookie = "ukcsid=" + e.target.value.replace( /ukcsid=/g, "" )
   document.querySelector( '#cookie-value' ).innerHTML = document.cookie
 } )
+
+document.addEventListener( 'DOMContentLoaded', () => {
+  document.querySelector( '#cookie-value' ).innerHTML = document.cookie
+} )
