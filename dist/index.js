09403246f6eb3cf1984b228150be25f4548097b5
fatboy <1241475+ryecroft@users.noreply.github.com>
2023-02-21 10:27:26 +0100

    updatezz

--- dist/index.js
+++ dist/index.js
@@ -12402,12 +12402,6 @@ let FiltersControllerElement = class extends BaseCon$1 {
       this.main_container.style.transform = "translate(0px, 0px)";
       this.backing_view.style.height = `${this.main_container.clientHeight - 30}px`;
     }, 10);
-    setTimeout(() => {
-      this.scroll_container.style.overflowY = "hidden";
-      setTimeout(() => {
-        this.scroll_container.style.overflowY = "scroll";
-      }, 100);
-    }, 100);
   }
   loadFilters() {
     const data = this.data;
